b9877e1dd5cf96d3c8bad90ea280407efb3c1067
aram-father <aram_father@naver.com>
2022-01-12 12:35:00 +0900

    feat: Add __init__.py for actfunc pacakge

---
+++ mnistetude/actfunc/__init__.py
@@ -0,0 +1,7 @@
+from .identity import identity
+from .relu import relu
+from .sigmoid import sigmoid
+from .softmax import softmax
+from .step import step
+
+__all__ = ['identity', 'relu', 'sigmoid', 'softmax', 'step']
