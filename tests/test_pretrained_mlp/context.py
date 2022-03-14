ae2c3ce03a9b3c610e73afb84cc623fb8251743b
aram-father <aram_father@naver.com>
2022-03-14 15:03:51 +0900

    test: Modify to use more specific usage of the classes/functions
    Use hierarchical expression for the claasses and functions to be clear to user.

--- tests/test_pretrained_mlp/context.py
+++ tests/test_pretrained_mlp/context.py
@@ -3,6 +3,3 @@ import sys
 
 sys.path.insert(0, path.abspath(path.dirname(__file__)))
 sys.path.insert(0, path.abspath(path.join(path.dirname(__file__), '../..')))
-
-import loader
-import mnistetude
--- tests/test_pretrained_mlp/test.py
+++ tests/test_pretrained_mlp/test.py
@@ -1,14 +1,28 @@
 import numpy as np
 from PIL import Image
-from context import loader, mnistetude
+import context
+import loader
+import mnistetude
+
 
 if __name__ == '__main__':
     dataset = loader.load_dataset(normalize=True)
-    parameters = loader.load_parameters()
+    params = loader.load_parameters()
 
-    network = mnistetude.Mlp(parameters)
+    network = mnistetude.network.MultiLayerPerceptron([
+        mnistetude.layer.Perceptron(params['input_layer_weights'],
+                                    params['input_layer_biases'],
+                                    mnistetude.actfunc.Sigmoid()),
+        mnistetude.layer.Perceptron(params['hidden_layer_weights'],
+                                    params['hidden_layer_biases'],
+                                    mnistetude.actfunc.Sigmoid()),
+        mnistetude.layer.Perceptron(params['output_layer_weights'],
+                                    params['output_layer_biases'],
+                                    mnistetude.actfunc.Softmax())
+    ])
 
-    batch_predictions = np.argmax(network.predict(dataset['test_images']), axis=1)
+    batch_predictions = np.argmax(
+        network.predict(dataset['test_images']), axis=1)
     acc = np.sum(batch_predictions == dataset['test_labels'])
 
     print(f'Accuracy: {float(100 * acc) / loader.number_of_test_images} %')
