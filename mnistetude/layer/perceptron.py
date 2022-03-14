cf95a138260c3e13b606457c4a6349784f9fd9e2
aram-father <aram_father@naver.com>
2022-03-14 15:00:55 +0900

    feat: Modify to use act. function interface rather than callable

--- mnistetude/layer/perceptron.py
+++ mnistetude/layer/perceptron.py
@@ -1,13 +1,13 @@
 import numpy as np
-from typing import Callable
+from .. import actfunc
 
 
 class Perceptron:
-    def __init__(self, weight: np.array, bias: np.array, actfunc: Callable):
+    def __init__(self, weight: np.array, bias: np.array, actfunc: actfunc.IActFunc):
         self.num_of_inputs, self.num_of_outputs = weight.shape
         self.weight = weight
         self.bias = bias
         self.actfunc = actfunc
 
     def forward(self, inputs: np.array) -> np.array:
-        return self.actfunc(inputs.dot(self.weight) + self.bias)
+        return self.actfunc.activate(inputs.dot(self.weight) + self.bias)
